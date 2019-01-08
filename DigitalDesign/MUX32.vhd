b198db8ff0bf7595440e651a225938340438e882
flottt <jana77go@gmail.com>
2019-01-08 07:39:05 +0100

    DD Fragen

---
+++ DigitalDesign/MUX32.vhd
@@ -0,0 +1,37 @@
+--------------------------------------------------------------------------------
+-- HEIG-VD, institute REDS, 1400 Yverdon-les-Bains
+-- Project :
+-- File    :
+-- Autor   :
+-- Date    :
+--
+--------------------------------------------------------------------------------
+-- Description :
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity MUX32 is
+  port(
+  ------------------------------------------------------------------------------
+  --Insert input ports below
+    enabled    : in  std_logic := 1;               
+    x0         : in  std_logic_vector(31 downto 0); 
+  ------------------------------------------------------------------------------
+  --Insert output ports below
+    result     : out std_logic_Vector(31 downto 0)
+    );
+end MUX32;
+
+--------------------------------------------------------------------------------
+--Complete your VHDL description below
+architecture MUXING32 of MUX32 is
+
+
+begin
+  result <= x0 when enabled = 1 else '0'; 
+
+end MUXING32;
